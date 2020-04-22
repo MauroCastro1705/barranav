5e372096c77e41f5b275bad81cf438f8882bc91b
Mauro Castro <49006241+MauroCastro1705@users.noreply.github.com>
2020-04-22 20:50:19 -0300

    Updates

---
+++ precache-manifest.2d5009df14ae56fd303df2b19d3307fe.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "846fc804d49b1540d1b79e5e557628bf",
+    "url": "/barranav/index.html"
+  },
+  {
+    "revision": "5ef876e245b12e85c008",
+    "url": "/barranav/static/css/main.98bbbb82.chunk.css"
+  },
+  {
+    "revision": "09e2e884a35375495be4",
+    "url": "/barranav/static/js/2.184b4b47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/barranav/static/js/2.184b4b47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ef876e245b12e85c008",
+    "url": "/barranav/static/js/main.567a840b.chunk.js"
+  },
+  {
+    "revision": "85e6c6f6c82cbecfd92f",
+    "url": "/barranav/static/js/runtime-main.1ace6882.js"
+  },
+  {
+    "revision": "48ae6434de48325549277b49f7ebf731",
+    "url": "/barranav/static/media/arrow.48ae6434.svg"
+  },
+  {
+    "revision": "e9fd40a70989ed93a960628beaf25b7d",
+    "url": "/barranav/static/media/bell.e9fd40a7.svg"
+  },
+  {
+    "revision": "681afa2ed6c043ea96751e7b755cc561",
+    "url": "/barranav/static/media/bolt.681afa2e.svg"
+  },
+  {
+    "revision": "615e14d18e96dc6cb0e028991a60ef80",
+    "url": "/barranav/static/media/caret.615e14d1.svg"
+  },
+  {
+    "revision": "efbb2bd6e6e3066e53de373d26c1e039",
+    "url": "/barranav/static/media/chevron.efbb2bd6.svg"
+  },
+  {
+    "revision": "993f0cc0e8de3fe0490e28fad0ee23a3",
+    "url": "/barranav/static/media/cog.993f0cc0.svg"
+  },
+  {
+    "revision": "9fdc8d67348585808e061c4755bb68f9",
+    "url": "/barranav/static/media/messenger.9fdc8d67.svg"
+  },
+  {
+    "revision": "9311af2e3926a973133dc6ebe8e8a9aa",
+    "url": "/barranav/static/media/plus.9311af2e.svg"
+  }
+]);
